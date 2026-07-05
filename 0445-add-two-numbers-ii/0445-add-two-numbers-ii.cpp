dffbd995db27c8120ad2dc0b510d9158694c8261
Ayushmann001 <ayushmanyadav424@gmail.com>
2026-07-05 13:13:41 +0530

    Time: 5 ms (22.01%), Space: 76.9 MB (82.5%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -0,0 +1,103 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    void insertattail(ListNode* &head, ListNode* &tail,int val){
+        ListNode* temp=new ListNode(val);
+        if(head==NULL){
+            head=temp;
+            tail=temp;
+            return;
+        }
+        else{
+            tail->next=temp;
+            tail=temp;
+        }
+
+    }
+    ListNode* add(ListNode* l1, ListNode* l2){
+        int carry=0;
+
+        ListNode* anshead=NULL;
+        ListNode* anstail=NULL;
+
+     while(l1!=NULL && l2!=NULL){
+
+       
+         int sum=carry+l1->val+l2->val;
+        int digit=sum%10;
+
+       insertattail(anshead,anstail,digit);
+        carry=sum/10;
+        
+        l1=l1->next;
+        l2=l2->next;
+
+     }
+     while(l1!=NULL){
+           int sum=carry+l1->val;
+            int digit=sum%10;
+
+            insertattail(anshead,anstail,digit);
+            carry=sum/10;
+            l1=l1->next;
+
+     }
+
+     while(l2!=NULL){
+           int sum=carry+l2->val;
+            int digit=sum%10;
+
+            insertattail(anshead,anstail,digit);
+            carry=sum/10;
+            l2=l2->next;
+
+     }
+     if(carry!=0){
+          
+            insertattail(anshead,anstail,carry);
+            
+
+     }
+        
+     return anshead;
+    }
+    ListNode* reverse(ListNode* head){
+        if(head==NULL)
+        return NULL;
+        if(head->next==NULL)
+        return head;
+
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        ListNode* next=NULL;
+
+        while(curr!=NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+
+    }
+    
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1=reverse(l1);
+        l2=reverse(l2);
+
+        ListNode* ans=add(l1,l2);
+
+        ans=reverse(ans);
+
+        return ans;
+    }
+};
