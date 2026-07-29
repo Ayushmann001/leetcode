abb8f88cffe24a1b0e6ec7f823e8acb66bec1715
Ayushmann001 <ayushmanyadav424@gmail.com>
2026-07-30 02:17:34 +0530

    Time: 4 ms (46.61%), Space: 23.6 MB (47.26%) - LeetHub

---
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -0,0 +1,89 @@
+class MyCircularQueue {
+    int *arr;
+    int size;
+    int front;
+    int rear;
+public:
+    MyCircularQueue(int k) {
+        size=k;
+        arr=new int[size];
+        front=rear=-1;
+    }
+    
+    bool enQueue(int value) {
+        if((front==0 && rear==size-1)||(rear==(front-1+size)%(size)))  // condn of full
+        return false;
+        else if(front==-1){ //first element to push
+            rear=front=0;
+        }
+        else if(rear==size-1 && front!=0){
+            rear=0;
+        }
+        else{
+            rear++;
+        }
+        arr[rear]=value;
+        return true;
+    }
+    
+    bool deQueue() {
+        if(front==-1)       //empty
+        return false;
+
+        int ans=arr[front];
+        arr[front]=-1;
+
+        if(front==rear)
+        front=rear=-1;
+
+        else if(front==size-1)
+        front=0;
+
+        else
+        front++;
+
+        return true;
+
+    }
+    
+    int Front() {
+        if(isEmpty())
+        return -1;
+        else
+        return arr[front];
+    }
+    
+    int Rear() {
+        if(isEmpty())
+        return -1;
+        else
+        return arr[rear];
+    }
+    
+    bool isEmpty() {
+        if(front==-1)
+        return true;
+
+        else
+        return false;
+    }
+    
+    bool isFull() {
+        if((front==0 && rear==size-1)||(rear==(front-1+size)%(size)))
+        return true;
+
+        else
+        return false;
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
