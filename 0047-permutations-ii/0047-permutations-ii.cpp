ac2377833f13729b848b4c0d7ca9530accf873eb
Ayushmann001 <ayushmanyadav424@gmail.com>
2026-08-07 19:55:48 +0530

    Time: 0 ms (100%), Space: 11.2 MB (98.19%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> p;
-        vector<int>s;
         sort(nums.begin(),nums.end());
-        s=nums;
         p.push_back(nums);
         while( next_permutation(nums.begin(),nums.end())){
            
