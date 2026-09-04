cf9cecaf1e976f8c6ffaf1a5c3b8c9ec93dffb8a
Ayushmann001 <ayushmanyadav424@gmail.com>
2026-09-04 15:24:36 +0530

    Time: 0 ms (100%), Space: 31.5 MB (36.87%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,37 +1,63 @@
 class Solution {
-    int maxx(vector<int>nums,int j){
-        int n=nums.size();
-        int maxi=INT_MIN;
-        for(int i=0;i<=j;i++){
-        int p=nums[i];
-        maxi=max(maxi,p);
-        }
-        return maxi;
-    }
-    int minn(vector<int>nums,int j){
-        int n=nums.size();
-        int mini=INT_MAX;
-        for(int i=j;i<n;i++){
-        int p=nums[i];
-        mini=min(mini,p);
-        }
-        return mini;
-    }
-    
 public:
-
     int firstStableIndex(vector<int>& nums, int k) {
-        int index=-1;
-        int score=0;
-        for(int i=0;i<nums.size();i++){
-            int p=maxx(nums,i);
-            int q=minn(nums,i);
+        int n=nums.size();
+        vector<int>maxx(n);
+        vector<int>minn(n);
 
-            int diff=p-q;
-            if(p-q <=k)
+        maxx[0]=nums[0];
+        minn[n-1]=nums[n-1];
+
+        for(int i=1;i<n;i++){
+            maxx[i]=max(maxx[i-1],nums[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            minn[i]=min(minn[i+1],nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            if(maxx[i]-minn[i]<=k)
             return i;
         }
-        
         return -1;
     }
 };
+
+
+
+// class Solution {
+//     int maxx(vector<int>nums,int j){
+//         int n=nums.size();
+//         int maxi=INT_MIN;
+//         for(int i=0;i<=j;i++){
+//         int p=nums[i];
+//         maxi=max(maxi,p);
+//         }
+//         return maxi;
+//     }
+//     int minn(vector<int>nums,int j){
+//         int n=nums.size();
+//         int mini=INT_MAX;
+//         for(int i=j;i<n;i++){
+//         int p=nums[i];
+//         mini=min(mini,p);
+//         }
+//         return mini;
+//     }
+    
+// public:
+
+//     int firstStableIndex(vector<int>& nums, int k) {
+//         int index=-1;
+//         int score=0;
+//         for(int i=0;i<nums.size();i++){
+//             int p=maxx(nums,i);
+//             int q=minn(nums,i);
+
+//             int diff=p-q;
+//             if(p-q <=k)
+//             return i;
+//         }
+        
+//         return -1;
+//     }
+// };
